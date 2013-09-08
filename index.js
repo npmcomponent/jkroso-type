9493a34d64768b431b10f965d9ff4a8ffd4c4244
Jake Rosoman <jkroso@gmail.com>
2013-09-08 13:27:54 +1200

    tabs

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 var toString = {}.toString
 var DomNode = typeof window != 'undefined'
-  ? window.Node
-  : Function
+	? window.Node
+	: Function
 
 /**
  * Return the type of `val`.
@@ -13,38 +13,38 @@ var DomNode = typeof window != 'undefined'
  */
 
 module.exports = exports = function(x){
-  var type = typeof x
-  if (type != 'object') return type
-  type = types[toString.call(x)]
-  if (type) return type
-  if (x instanceof DomNode) switch (x.nodeType) {
-    case 1:  return 'element'
-    case 3:  return 'text-node'
-    case 9:  return 'document'
-    case 11: return 'document-fragment'
-    default: return 'dom-node'
-  }
+	var type = typeof x
+	if (type != 'object') return type
+	type = types[toString.call(x)]
+	if (type) return type
+	if (x instanceof DomNode) switch (x.nodeType) {
+		case 1:  return 'element'
+		case 3:  return 'text-node'
+		case 9:  return 'document'
+		case 11: return 'document-fragment'
+		default: return 'dom-node'
+	}
 }
 
 var types = exports.types = {
-  '[object Function]': 'function',
-  '[object Date]': 'date',
-  '[object RegExp]': 'regexp',
-  '[object Arguments]': 'arguments',
-  '[object Array]': 'array',
-  '[object String]': 'string',
-  '[object Null]': 'null',
-  '[object Undefined]': 'undefined',
-  '[object Number]': 'number',
-  '[object Boolean]': 'boolean',
-  '[object Object]': 'object',
-  '[object Text]': 'text-node',
-  '[object Uint8Array]': '8bit-array',
-  '[object Uint16Array]': '16bit-array',
-  '[object Uint32Array]': '32bit-array',
-  '[object Uint8ClampedArray]': '8bit-array',
-  '[object Error]': 'error',
-  '[object FormData]': 'form-data',
-  '[object File]': 'file',
-  '[object Blob]': 'blob'
+	'[object Function]': 'function',
+	'[object Date]': 'date',
+	'[object RegExp]': 'regexp',
+	'[object Arguments]': 'arguments',
+	'[object Array]': 'array',
+	'[object String]': 'string',
+	'[object Null]': 'null',
+	'[object Undefined]': 'undefined',
+	'[object Number]': 'number',
+	'[object Boolean]': 'boolean',
+	'[object Object]': 'object',
+	'[object Text]': 'text-node',
+	'[object Uint8Array]': '8bit-array',
+	'[object Uint16Array]': '16bit-array',
+	'[object Uint32Array]': '32bit-array',
+	'[object Uint8ClampedArray]': '8bit-array',
+	'[object Error]': 'error',
+	'[object FormData]': 'form-data',
+	'[object File]': 'file',
+	'[object Blob]': 'blob'
 }
